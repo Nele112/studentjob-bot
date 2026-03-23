a2440b827835a22040b33964e37dc9f397426bd8
Jani Partanen <jani.a.partanen@hotmail.com>
2026-03-23 02:41:15 +0200

    test

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from robocorp.tasks import task
-
+from robocorp import browser
+from RPA.Excel.Files import Files
 @task
-def minimal_task():
+def student_job_robot():
     print ("Let's start with our project!")
