dfa627bb0a47d89f2c4d1717d1016c3a65324b77
Jani Partanen <jani.a.partanen@hotmail.com>
2026-03-23 03:27:53 +0200

    Placeholder excel file and some main task code

--- tasks.py
+++ tasks.py
@@ -3,4 +3,19 @@ from robocorp import browser
 from RPA.Excel.Files import Files
 @task
 def student_job_robot():
-    print ("Let's start with our project!")
+    browser.configure(
+        slowmo = 100,
+    )
+    search_linkedin()
+    extract_data()
+    compare_data()
+
+def compare_data():
+    open_data_excel()
+    compare_jobs()
+    write_new_jobs()
+    notify_user_by_email()
+    
+def open_data_excel():
+    lib = Files()
+    lib.open_workbook(path="data.xlsx")
